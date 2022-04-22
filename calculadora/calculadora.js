3fb5b1b864b036e5cd0bbfc7b0eac16b129f5282
henriquecesar139 <henriquepb139@gmail.com>
2022-04-22 17:36:24 -0300

    calculadora feita em javascript

---
+++ calculadora/calculadora.js
@@ -0,0 +1,8 @@
+//Muda os valores das variáveis para obter outros resultados
+n1 = 10
+n2 = 5
+
+console.log(n1, "+", n2, "=", n1 + n2)
+console.log(n1, "-", n2, "=", n1 - n2)
+console.log(n1, "*", n2, "=", n1 * n2)
+console.log(n1, "/", n2, "=", n1 / n2)
