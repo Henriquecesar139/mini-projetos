70360aca6d573cc865180128503a316fb1d17d25
henriquecesar139 <henriquepb139@gmail.com>
2022-04-22 17:36:34 -0300

    calculadora feita em python

---
+++ calculadora/calculadora.py
@@ -0,0 +1,8 @@
+n1 = int (input('Digite um numero: '))
+
+n2 = int (input('Digite outro numero: '))
+
+print(f'{n1} + {n2} = {n1 + n2}')
+print(f'{n1} - {n2} = {n1 - n2}')
+print(f'{n1} * {n2} = {n1 * n2}')
+print(f'{n1} / {n2} = {n1 / n2}')
